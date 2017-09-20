1bf791afb2ae598adffeb64c7958daaa883de4b2
Stephen Callan <stephencallan@gmail.com>
2017-09-20 14:10:18 +0100

    Adding new method for checking for Palendromes

--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -13,6 +13,33 @@ namespace Fibonacci
             Console.WriteLine("First change");
             Console.WriteLine("Second change bugfixing");
             Console.WriteLine("Third change bugfixing");
+
+            var result = IsPalendrome("kayal");
+        }
+
+        public static bool IsPalendrome(string input)
+        {
+            if (input.Length == 1)
+            {
+                return true;
+            }
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            var chararray = input.ToCharArray();
+
+            for (int i = 0; i < input.Length/2; i++)
+            {
+                if (chararray[i] != chararray[input.Length -i -1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
